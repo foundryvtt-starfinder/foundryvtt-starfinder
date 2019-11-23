287b8f4e196ec057e1495caa8e0c6aee75df1d13
James Allred <wildj79@gmail.com>
2019-11-22 20:44:49 -0600

    Added license and updated gulpfile to remove js concatenation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,6 @@
 
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const less = require('gulp-less');
-
-//const STARFINDER_SCRIPTS = ["scripts/**/*.js"];
-
-// function concatScripts() {
-//     return gulp.src(STARFINDER_SCRIPTS)
-//         .pipe(concat('starfinder.js'))
-//         .pipe(gulp.dest('./'));
-// }
-
-//const js = gulp.series(concatScripts);
 
 const STARFINDER_LESS = ["less/*.less"];
 
@@ -24,10 +13,8 @@ function compileLESS() {
 const css = gulp.series(compileLESS);
 
 function watchUpdates() {
-    //gulp.watch(STARFINDER_SCRIPTS, js);
     gulp.watch(STARFINDER_LESS, css);
 }
 
 exports.default = gulp.series(css, watchUpdates);
-//exports.js = js;
 exports.css = css;
