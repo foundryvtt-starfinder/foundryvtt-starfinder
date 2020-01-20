1855f7460f797b68c225d1e9eb8511fb4e0475bc
James Allred <wildj79@gmail.com>
2020-01-19 19:47:25 -0600

    Fixes issue where users on small resolutions couldn't see the whole sheet. This is an imperfect solution, but it's an easy one to make so that everyone can use the sheet for now. My intention is to redesign the sheet a little, so that it is wider than it is taller, and will probably move some information to different tabs.

--- module/actor/sheet/character.js
+++ module/actor/sheet/character.js
@@ -6,7 +6,7 @@ export class ActorSheetStarfinderCharacter extends ActorSheetStarfinder {
         mergeObject(options, {
             classes: ['starfinder', 'sheet', 'actor', 'character'],
             width: 715,
-            height: 830
+            //height: 830
         });
 
         return options;
