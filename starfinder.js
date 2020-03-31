cb33f1c8f731bc689ad5b4aea88b15abd8e8ae3e
James Allred <wildj79@gmail.com>
2020-03-30 23:15:50 -0500

    Prepping Starfinder for next release.

--- starfinder.js
+++ starfinder.js
@@ -23,6 +23,15 @@ import { _getInitiativeFormula, addChatMessageContextOptions } from "./module/co
 
 Hooks.once('init', async function () {
     console.log(`Starfinder | Initializeing Starfinder System`);
+    console.log(
+`__________________________________________________
+ ____  _              __ _           _
+/ ___|| |_ __ _ _ __ / _(_)_ __   __| | ___ _ __
+\\___ \\| __/ _\` | '__| |_| | '_ \\ / _\` |/ _ \\ '__|
+ ___) | || (_| | |  |  _| | | | | (_| |  __/ |
+|____/ \\__\\__,_|_|  |_| |_|_| |_|\\__,_|\\___|_|
+==================================================`
+    );
 
     game.starfinder = {
         rollItemMacro
