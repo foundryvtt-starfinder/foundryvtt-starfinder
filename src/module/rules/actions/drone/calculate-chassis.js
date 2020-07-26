e9b99e6ddd9294b83421464bac24e769c759b076
Tim Toxopeus <tim.toxopeus@gmail.com>
2020-07-26 17:33:46 +0200

    * Removed bonus skill unit and initial mods fields from chassis, as all content for it can now be handled by a custom drone mod instead.

--- src/module/rules/actions/drone/calculate-chassis.js
+++ src/module/rules/actions/drone/calculate-chassis.js
@@ -57,29 +57,6 @@ export default function (engine) {
             data.skills[skill].tooltip = [];
         }
 
-        if (activeChassis.data.bonusSkillUnit) {
-            let skill = data.skills[activeChassis.data.bonusSkillUnit];
-            skill.enabled = true;
-            skill.value = 3;
-            skill.ranks = droneLevel;
-
-            let tooltip = game.i18n.format("SFRPG.SkillModifierTooltip", {
-                type: "Class Skill",
-                mod: skill.value.signedString(),
-                source: activeChassis.name
-            });
-
-            skill.tooltip.push(tooltip);
-
-            tooltip = game.i18n.format("SFRPG.SkillModifierTooltip", {
-                type: "Mechanic Level",
-                mod: skill.ranks.signedString(),
-                source: activeChassis.name
-            });
-
-            skill.tooltip.push(tooltip);
-        }
-
         return fact;
     }, { required: ["stackModifiers"], closureParameters: ["stackModifiers"] });
 }
