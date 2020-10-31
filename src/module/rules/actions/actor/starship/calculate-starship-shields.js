7543c85b4ee77749003a9f11c97066c762fd77f7
Tim Toxopeus <tim.toxopeus@gmail.com>
2020-10-31 14:33:46 +0100

    * Updated and localized shields display.

--- src/module/rules/actions/actor/starship/calculate-starship-shields.js
+++ src/module/rules/actions/actor/starship/calculate-starship-shields.js
@@ -1,8 +1,10 @@
 export default function(engine) {
     engine.closures.add("calculateStarshipShields", (fact, context) => {
         const data = fact.data;
+        const shields = data.attributes.shields;
 
-        data.attributes.shields.max = CONFIG.SFRPG.shieldsMap[data.details.systems.shields] || 0;
+        shields.max = CONFIG.SFRPG.shieldsMap[data.details.systems.shields] || 0;
+        shields.value = shields.forward.value + shields.aft.value + shields.starboard.value + shields.port.value;
 
         return fact;
     });
