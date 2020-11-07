ad53ee220cbef4923b69d4b09aca8f65646f858d
Tim Toxopeus <tim.toxopeus@gmail.com>
2020-11-07 18:08:36 +0100

    * Fixing starship calculations freezing up until a pilot is present.

--- src/module/rules/actions/actor/starship/calculate-starship-ac.js
+++ src/module/rules/actions/actor/starship/calculate-starship-ac.js
@@ -2,7 +2,9 @@ export default function(engine) {
     engine.closures.add("calculateStarshipArmorClass", (fact, context) => {
         const data = fact.data;
 
-        const pilotingBonus = data.crew.pilot.actors[0]?.data?.data?.skills?.pil?.ranks || 0;
+        const pilot = (data.crew?.pilot?.actors) ? data.crew?.pilot?.actors[0] : null;
+
+        const pilotingBonus = pilot?.data?.data?.skills?.pil?.ranks || 0;
         const defenses = CONFIG.SFRPG.armorDefenseMap[data.details.systems.armor] || 0;
         const sizeMod = CONFIG.SFRPG.starshipSizeMod[data.details.size] || 0;
         const misc = data.attributes.ac?.misc || 0;
