e5d79a7f95d41eb114ea343819d4390bef30ffce
CptTwinkie <cew182cz@gmail.com>
2021-01-01 16:20:42 +0100

    AC calculations now correctly handle multiple shields equipped at the same time

--- src/module/rules/actions/actor/calculate-base-armor-class.js
+++ src/module/rules/actions/actor/calculate-base-armor-class.js
@@ -15,15 +15,15 @@ export default function (engine) {
 
         if (armor || shields) {
             // Max dex
-            const shield = shields?.sort((a, b) => a.data.dex <= b.data.dex ? -1 : 1)[0];
-            let maxShieldDex = shield?.data.dex ?? Number.MAX_SAFE_INTEGER;
+            const shieldMinDex = shields?.sort((a, b) => a.data.dex <= b.data.dex ? -1 : 1)[0];
+            let maxShieldDex = shieldMinDex?.data.dex ?? Number.MAX_SAFE_INTEGER;
             let maxArmorDex = armor?.data.armor.dex ?? Number.MAX_SAFE_INTEGER;
 
             const maxDex = Math.min(data.abilities.dex.mod, maxArmorDex, maxShieldDex);
             const maxDexTooltip = game.i18n.format("SFRPG.ACTooltipMaxDex", { 
                 maxDex: maxDex.signedString(), 
                 armorMax: armor?.data.armor.dex?.signedString() ?? game.i18n.localize("SFRPG.Items.Unlimited"),
-                shieldMax: shield?.data.dex?.signedString() ?? game.i18n.localize("SFRPG.Items.Unlimited")
+                shieldMax: shieldMinDex?.data.dex?.signedString() ?? game.i18n.localize("SFRPG.Items.Unlimited")
             });
 
             // AC bonuses
@@ -38,14 +38,17 @@ export default function (engine) {
                 kac.tooltip.push(notProfTooltip);
             }
 
-            let shieldBonus = 0;
-            if (shield) {
-                let shieldProfBonus = shield.data.bonus.wielded;
+            let shieldBonus       = 0;
+            let totalShieldBonus  = 0;
+            
+            if (shields) {
+                shields.forEach(shield => {
+                    totalShieldBonus += shield.data.bonus.wielded;
+                    if (shield.data.proficient) shieldBonus += shield.data.bonus.wielded;
+                });
 
-                if (shield.data.proficient) {
-                    shieldBonus = shieldProfBonus;
-                } else {
-                    const shieldNotProfTooltip = game.i18n.format("SFRPG.ACTooltipNotProficientShield", { profMod: -shieldProfBonus });
+                if (shieldBonus !== totalShieldBonus) {
+                    const shieldNotProfTooltip = game.i18n.format("SFRPG.ACTooltipNotProficientShield", { profMod: shieldBonus - totalShieldBonus });
                     eac.tooltip.push(shieldNotProfTooltip);
                     kac.tooltip.push(shieldNotProfTooltip);
                 }
@@ -59,11 +62,11 @@ export default function (engine) {
             kac.value = 10 + kacMod;
             
             if (armor) eac.tooltip.push(game.i18n.format("SFRPG.ACTooltipArmorACMod", { armor: armorEac.signedString(), name: armor.name }));
-            if (shield) eac.tooltip.push(game.i18n.format("SFRPG.ACTooltipShieldACMod", { shield: shield.data.bonus.wielded.signedString(), name: shield.name }));
+            if (shields) shields.forEach(shield => eac.tooltip.push(game.i18n.format("SFRPG.ACTooltipShieldACMod", { shield: shield.data.bonus.wielded.signedString(), name: shield.name })));
             eac.tooltip.push(maxDexTooltip);
 
             if (armor) kac.tooltip.push(game.i18n.format("SFRPG.ACTooltipArmorACMod", { armor: armorKac.signedString(), name: armor.name }));
-            if (shield) kac.tooltip.push(game.i18n.format("SFRPG.ACTooltipShieldACMod", { shield: shield.data.bonus.wielded.signedString(), name: shield.name }));
+            if (shields) shields.forEach(shield => kac.tooltip.push(game.i18n.format("SFRPG.ACTooltipShieldACMod", { shield: shield.data.bonus.wielded.signedString(), name: shield.name })));
             kac.tooltip.push(maxDexTooltip);
         } else {
             eac.value = 10 + data.abilities.dex.mod;
