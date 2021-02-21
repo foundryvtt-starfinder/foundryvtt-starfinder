b7fda3b51947002d846aba32dfe6daeed71ca7bb
Ryan Renna <rrenna360@gmail.com>
2021-02-21 09:40:45 -0500

    Basic migration step for vehicles without crew.

--- src/module/rules/actions/actor/vehicle/calculate-vehicle-passengers.js
+++ src/module/rules/actions/actor/vehicle/calculate-vehicle-passengers.js
@@ -2,6 +2,23 @@ export default function (engine) {
     engine.closures.add("calculateVehiclePassengers", (fact, context) => {
         const data = fact.data;
 
+        data.crew = mergeObject(data.crew, {
+
+            complement: {
+                limit: 0,
+                actorIds: []
+            },
+            passenger: {
+                limit: 0,
+                actorIds: []
+            },
+            pilot: {
+                limit: 1,
+                actorIds: []
+            },
+            useNPCCrew: false
+        }, {overwrite: false});
+
         for (let [key, crew] of Object.entries(data.crew)) {
             if (key === "useNPCCrew") {
                 continue;
