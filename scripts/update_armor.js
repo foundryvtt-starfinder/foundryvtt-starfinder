3cadd392020a666c8c58c6371d12e55aabd63b53
Tim Toxopeus <tim.toxopeus@gmail.com>
2021-12-28 17:23:12 +0100

    * update_armor no longer updates armors if they are already correct.

--- scripts/update_armor.js
+++ scripts/update_armor.js
@@ -17,14 +17,25 @@ try {
             const itemData = JSON.parse(json);
             
             if (itemData.type === "equipment") {
-                itemData.data.equippable = true;
-                itemData.data.proficient = false;
-
-                const output = JSON.stringify(itemData, null, 2);
-
-                fs.writeFileSync(`${dataPath}/${file}`, output);
+                let isDirty = false;
                 
-                count += 1;
+                if (!itemData.data.equippable) {
+                    itemData.data.equippable = true;
+                    isDirty = true;
+                }
+                
+                if (itemData.data.proficient) {
+                    itemData.data.proficient = false;
+                    isDirty = true;
+                }
+
+                if (isDirty) {
+                    const output = JSON.stringify(itemData, null, 2);
+
+                    fs.writeFileSync(`${dataPath}/${file}`, output);
+                    
+                    count += 1;
+                }
             }
         }
         
