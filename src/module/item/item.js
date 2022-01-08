b6c8f97da7b320981e630d4e5d03eb31eec22881
Tim Toxopeus <tim.toxopeus@gmail.com>
2022-01-08 23:23:44 +0100

    [DMG] * Fixed vehicle and ship damage rolls not grabbing the right data.

--- src/module/item/item.js
+++ src/module/item/item.js
@@ -1094,25 +1094,10 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
             ui.notifications.error(game.i18n.localize("SFRPG.VehicleAttackSheet.Errors.NoDamage"))
         }
 
-        // const [parts, damageTypes] = itemData.damage.parts.reduce((acc, cur) => {
-        //     if (cur.formula && cur.formula.trim() !== "") acc[0].push(cur.formula);
-        //     if (cur.types) {
-        //         const filteredTypes = Object.entries(cur.types).filter(type => type[1]);
-        //         const obj = { types: [], operator: "" };
-
-        //         for (const type of filteredTypes) {
-        //             obj.types.push(type[0]);
-        //         }
-
-        //         if (cur.operator) obj.operator = cur.operator;
-
-        //         acc[1].push(obj);
-        //     }
-
-        //     return acc;
-        // }, [[], []]);
-
         const parts = itemData.damage.parts.map(part => part);
+        for (const part of parts) {
+            part.isDamageSection = true;
+        }
 
         let title = '';
         if (game.settings.get('sfrpg', 'useCustomChatCards')) {
@@ -1156,6 +1141,9 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
         }
 
         const parts = itemData.damage.parts.map(part => part);
+        for (const part of parts) {
+            part.isDamageSection = true;
+        }
 
         let title = '';
         if (game.settings.get('sfrpg', 'useCustomChatCards')) {
