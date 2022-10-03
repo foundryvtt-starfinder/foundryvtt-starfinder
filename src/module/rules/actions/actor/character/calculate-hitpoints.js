1715642c53dc2f012b2b636400c11b0a4338fca9
James Allred <wildj79@gmail.com>
2022-10-02 21:05:17 -0500

    Updated rules/actions/actor/character/calculate-hitpoints.js to use the new v10 data model.

--- src/module/rules/actions/actor/character/calculate-hitpoints.js
+++ src/module/rules/actions/actor/character/calculate-hitpoints.js
@@ -37,7 +37,7 @@ export default function (engine) {
         // Race bonus
         if (fact.races && fact.races.length > 0) {
             for (const race of fact.races) {
-                const raceData = race.data.data;
+                const raceData = race.system;
 
                 hpMax += raceData.hp.value;
 
@@ -51,7 +51,7 @@ export default function (engine) {
         // Class bonus
         if (fact.classes && fact.classes.length > 0) {
             for (const cls of fact.classes) {
-                const classData = cls.data.data;
+                const classData = cls.system;
 
                 let classBonus = Math.floor(classData.levels * classData.hp.value);
                 hpMax += classBonus;
