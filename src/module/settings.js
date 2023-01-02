4d0ace0a5b459488e30cba3ccfbd61cd55be9f7b
LebombJames <77904738+LebombJames@users.noreply.github.com>
2023-01-02 03:49:26 +0000

    Fix floating number not having defaults, and move sheet class (#898)

--- src/module/settings.js
+++ src/module/settings.js
@@ -1,5 +1,6 @@
 import { SFRPG } from "./config.js";
 import { ItemSFRPG } from "./item/item.js";
+import FloatingNumberMenu from "./classes/floating-number-menu.js";
 
 export const registerSystemSettings = function() {
     game.settings.register("sfrpg", "diagonalMovement", {
@@ -231,7 +232,7 @@ export const registerSystemSettings = function() {
         label: "SFRPG.Settings.FloatingHP.Button",
         hint: "SFRPG.Settings.FloatingHP.Hint",
         icon: "fas fa-heart",
-        type: floatingNumberMenu
+        type: FloatingNumberMenu
     });
 
     game.settings.register("sfrpg", "floatingHP", {
@@ -258,13 +259,15 @@ export const registerSystemSettings = function() {
     game.settings.register("sfrpg", "canSeeBars", {
         scope: "world",
         config: false,
-        type: Boolean
+        type: Boolean,
+        default: false
     });
 
     game.settings.register("sfrpg", "canSeeName", {
         scope: "world",
         config: false,
-        type: Boolean
+        type: Boolean,
+        default: false
     });
 
     game.settings.register("sfrpg", "minPerm", {
@@ -274,51 +277,3 @@ export const registerSystemSettings = function() {
         default: "LIMITED"
     });
 };
-
-class floatingNumberMenu extends FormApplication {
-    constructor(...args) {
-        super(...args);
-    }
-
-    getData() {
-        let data = super.getData();
-        data.perms = {
-            "LIMITED": "OWNERSHIP.LIMITED",
-            "OBSERVER": "OWNERSHIP.OBSERVER",
-            "OWNER": "OWNERSHIP.OWNER"
-        };
-
-        data.floatingToggle = game.settings.get("sfrpg", "floatingHP");
-        data.verboseFloatyText = game.settings.get("sfrpg", "verboseFloatyText");
-        data.limitByCriteria = game.settings.get("sfrpg", "limitByCriteria");
-        data.minPerm = game.settings.get("sfrpg", "minPerm");
-        data.canSeeName = game.settings.get("sfrpg", "canSeeName");
-        data.canSeeBars = game.settings.get("sfrpg", "canSeeBars");
-
-        return data;
-    }
-
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            classes: ['form'],
-            popOut: true,
-            template: `systems/sfrpg/templates/apps/floatinghp.hbs`,
-            id: 'floating-hp',
-            title: 'SFRPG.Settings.FloatingHP.Menu.Label',
-            width: 600
-        });
-    }
-
-    activateListeners(html) {
-        super.activateListeners(html);
-    }
-
-    async _updateObject(event, formData) {
-        await game.settings.set("sfrpg", "floatingHP", formData["floating-toggle"]);
-        await game.settings.set("sfrpg", "verboseFloatyText", formData["verbose-floaty-text"]);
-        await game.settings.set("sfrpg", "limitByCriteria", formData["limit-by-criteria"]);
-        await game.settings.set("sfrpg", "minPerm", formData["min-perm"]);
-        await game.settings.set("sfrpg", "canSeeName", formData["can-see-name"]);
-        await game.settings.set("sfrpg", "canSeeBars", formData["can-see-bars"]);
-    }
-}
