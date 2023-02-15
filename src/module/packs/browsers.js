ca77d330b1ac2e1d02671d504eba69090b08fdb7
LebombJames <sam.marine418@gmail.com>
2023-02-15 02:43:24 +0000

    Refactor browser buttons on compendium sidebar

--- src/module/packs/browsers.js
+++ src/module/packs/browsers.js
@@ -1,16 +1,16 @@
 import { getAlienArchiveBrowser } from './alien-archive-browser.js';
-import { getSpellBrowser } from './spell-browser.js';
 import { getEquipmentBrowser } from './equipment-browser.js';
+import { getSpellBrowser } from './spell-browser.js';
 import { getStarshipBrowser } from './starship-browser.js';
 
 Hooks.on('renderCompendiumDirectory', (app, html, data) => {
     // Browser Buttons
     const grouping = $('<div class="flexcol browser-group"></div>');
 
-    const alienArchiveBrowserButton = $(`<button class="alien-archive-browser-btn"><i class="fas fa-pastafarianism"></i> ${game.i18n.format("SFRPG.Browsers.AlienArchiveBrowser.Button")}</button>`);
-    const spellBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-fire"></i> ${game.i18n.format("SFRPG.Browsers.SpellBrowser.Button")}</button>`);
-    const equipmentBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-cog"></i> ${game.i18n.format("SFRPG.Browsers.EquipmentBrowser.Button")}</button>`);
-    const starshipBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-rocket"></i> ${game.i18n.format("SFRPG.Browsers.StarshipBrowser.Button")}</button>`);
+    const alienArchiveBrowserButton = $(`<button class="alien-archive-browser-btn"><i class="fas fa-alien"></i><br> ${game.i18n.format("SFRPG.Browsers.AlienArchiveBrowser.Button")}</button>`);
+    const spellBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-sparkles"></i><br> ${game.i18n.format("SFRPG.Browsers.SpellBrowser.Button")}</button>`);
+    const equipmentBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-gun"></i><br> ${game.i18n.format("SFRPG.Browsers.EquipmentBrowser.Button")}</button>`);
+    const starshipBrowserButton = $(`<button class="spell-browser-btn"><i class="fas fa-rocket"></i><br> ${game.i18n.format("SFRPG.Browsers.StarshipBrowser.Button")}</button>`);
 
     html.find('.directory-footer').append(grouping);
     html.find('.browser-group').append(alienArchiveBrowserButton);
