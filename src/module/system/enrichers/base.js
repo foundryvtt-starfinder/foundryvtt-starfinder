7406b7531f97a23999cd6498b2b3ac972f5aebcc
LebombJames <77904738+LebombJames@users.noreply.github.com>
2023-03-28 16:58:38 +0100

    Add framework for repost buttons on inline buttons (#1032)
    * Add framework for repost buttons on inline buttons
    
    * Update comments and private internal repost
    
    * Clean out some white space

--- src/module/system/enrichers/base.js
+++ src/module/system/enrichers/base.js
@@ -20,6 +20,12 @@ export default class BaseEnricher {
         this.enricher = this.enricherFunc.bind(this);
     }
 
+    /** --------
+    |           |
+    |  Getters  |
+    |           |
+    ----------*/
+
     /**
      * The RegExp to capture the text.
      * @returns {RegExp}
@@ -51,6 +57,12 @@ export default class BaseEnricher {
     get icons() {
         throw new Error("This method must be implemented on subclasses of BaseEnricher.");
     }
+
+    /** -------------------
+    |                      |
+    |  Element Generation  |
+    |                      |
+    ----------------------*/
 
     /**
      * Transform the Regex match array into an enriched element, performing validation.
@@ -124,7 +136,7 @@ export default class BaseEnricher {
      * @returns {HTMLAnchorElement}
      */
     createElement() {
-        const a = document.createElement("a");
+        let a = document.createElement("a");
 
         a.dataset.action = this.enricherType;
         a.dataset.type = this.args.type;
@@ -134,8 +146,57 @@ export default class BaseEnricher {
 
         a.innerText = this.name;
 
+        if (this.#_hasRepost) a = this.addRepost(a);
+
         return a;
     }
+
+    /** -------
+    |          |
+    |  Repost  |
+    |          |
+    -----------*/
+
+    /**
+     * Should this enricher have a repost button appended to created elements?
+     * Create both a publicly accessible static variable and an internal instance one.
+     * @type {Boolean}
+     */
+    static hasRepost = false;
+    /** @type {Boolean} */
+    #_hasRepost = this.constructor.hasRepost;
+
+    /**
+     * Take an anchor element and append a repost button
+     * @param {HTMLAnchorElement} a The original anchor
+     * @returns The inputted Anchor, with a repost button appended
+     */
+    addRepost(a) {
+        let repost = document.createElement("i");
+        repost.classList.add("fas", "fa-comment-alt", "repost");
+        repost.dataset.tooltip = "SFRPG.Enrichers.SendToChat";
+
+        a.append(repost);
+
+        return a;
+    }
+
+    /**
+     * Handle repost button click, sending a chat message of the current target to chat.
+     * @param {Event} event
+     * @returns Create a chat message
+     */
+    static repostListener(event) {
+        event.stopPropagation();
+
+        return ChatMessage.create({content: event.currentTarget.parentElement.outerHTML});
+    }
+
+    /** ---------
+    |            |
+    |  Listener  |
+    |            |
+    ------------*/
 
     /**
      * Whether the enricher has an event listener.
@@ -146,17 +207,18 @@ export default class BaseEnricher {
     /**
      * A callback function to run when the element is clicked.
      * @param {Event} event The DOM event that triggers the listener
-     * @returns {*}
+     * @returns {void}
      */
     static listener(event) {}
 
+    /**
+     * Add Event listeners to the DOM body at startup.
+     */
     static addListeners() {
         const body = $("body");
+        body.on("click", `i.repost`, this.repostListener);
         for (const [action, cls] of Object.entries(CONFIG.SFRPG.enricherTypes)) {
-            if (!cls.hasListener) continue;
-            const enricherListener = cls.listener;
-
-            body.on("click", `a[data-action="${action}"]`, enricherListener);
+            if (cls.hasListener) body.on("click", `a[data-action="${action}"]`, cls.listener);
         }
     }
 }
