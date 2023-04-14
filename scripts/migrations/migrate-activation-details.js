ee34f576ad0d9b19f75036295b7dde843e182e70
Sam Marine <sam.marine418@gmail.com>
2023-04-14 02:13:22 +0100

    Revert migration script to non-AA version

--- scripts/migrations/migrate-activation-details.js
+++ scripts/migrations/migrate-activation-details.js
@@ -8,7 +8,6 @@ const dataPaths = [
     "../../src/items/universal-creature-rules",
     "../../src/items/spells",
     "../../src/items/equipment" */
-    "../../src/items/alien-archives"
 ];
 
 // Migrate features and spells to use calculated values for range, area and duration
@@ -25,97 +24,94 @@ try {
                 });
                 let dirty = false;
 
-                const alien = JSON.parse(json);
-
-                for (const item of alien.items) {
+                const item = JSON.parse(json);
                 // Migrate range
-                    const range = item?.system?.range;
-                    if (range) {
-                        if (range?.additional?.includes("5 ft")
+                const range = item?.system?.range;
+                if (range) {
+                    if (range?.additional?.includes("5 ft")
                         && range?.per?.includes("2 levels")
                         && range?.units?.includes("ft")
                         && range?.value === 25) {
-                            range.units = "close";
-                            delete range.additional;
-                            delete range.per;
-                            delete range.value;
-                            dirty = true;
-                        } else if (range?.additional?.includes("10 ft")
+                        range.units = "close";
+                        delete range.additional;
+                        delete range.per;
+                        delete range.value;
+                        dirty = true;
+                    } else if (range?.additional?.includes("10 ft")
                         && range?.per?.includes("level")
                         && range?.units?.includes("ft")
                         && range?.value === 100) {
-                            range.units = "medium";
-                            delete range.additional;
-                            delete range.per;
-                            delete range.value;
-                            dirty = true;
-                        } else if (range?.additional?.includes("40 ft")
+                        range.units = "medium";
+                        delete range.additional;
+                        delete range.per;
+                        delete range.value;
+                        dirty = true;
+                    } else if (range?.additional?.includes("40 ft")
                         && range?.per?.includes("level")
                         && range?.units?.includes("ft")
                         && range?.value === 400) {
-                            range.units = "long";
-                            delete range.additional;
-                            delete range.per;
-                            delete range.value;
-                            dirty = true;
-                        }
-
-                        const duration = item?.system?.duration;
-
-                        // Make sure existing data is accurate
-                        if (duration?.value?.includes("(D)") && !item?.system?.dismissible) {
-                            item.system.dismissible = true;
-                            dirty = true;
-                        }
-
-                        let durationRegex = duration?.value?.match(/^(\d+) (round|minute|hour|day)[s]?(\/)?(\d*)? ?(level[s]?)?/);
-                        let seeText = duration?.value?.includes("see text");
-                        if (durationRegex && !seeText) {
-                            const [whole,
-                                amount,
-                                unit,
-                                slash,
-                                scaling,
-                                level] = durationRegex;
-                            let formula = "";
-                            if (slash) {
-                                formula = amount >  1 ? `@details.cl.value*${amount}` : `@details.cl.value`;
-                                if (scaling > 0) formula = `floor(${formula}/${scaling})`;
-                            } else {
-                                formula = amount;
-                            }
-                            const classMatch = duration?.value?.match(/per (biohacker|envoy|evolutionist|mechanic|mystic|nanocyte|operative|precog|solarian|soldier|technomancer|vanguard|witchwarper|character) level/i);
-                            if (classMatch?.[1] && classMatch[1] !== "character") {
-                                formula = `${formula}*@classes.${classMatch[1].toLowerCase()}.levels`;
-                            } else if (classMatch?.[1]) {
-                                formula = `${formula}*@details.level.value`;
-                            }
-                            duration.value = formula;
-                            duration.units = unit;
-                            dirty = true;
-                        }
-
-                        if (duration?.value === "instantaneous") {
-                            duration.units = "instantaneous";
-                            duration.value = "";
-                            dirty = true;
-                        }
-
-                        const area = item?.system.area;
-                        if (area?.shape === "shapable") {
-                            area.shape = "cube";
-                            area.shapable = true;
-                            dirty = true;
-                        }
-
+                        range.units = "long";
+                        delete range.additional;
+                        delete range.per;
+                        delete range.value;
+                        dirty = true;
                     }
+
+                    const duration = item?.system?.duration;
+
+                    // Make sure existing data is accurate
+                    if (duration?.value?.includes("(D)") && !item?.system?.dismissible) {
+                        item.system.dismissible = true;
+                        dirty = true;
+                    }
+
+                    let durationRegex = duration?.value?.match(/^(\d+) (round|minute|hour|day)[s]?(\/)?(\d*)? ?(level[s]?)?/);
+                    let seeText = duration?.value?.includes("see text");
+                    if (durationRegex && !seeText) {
+                        const [whole,
+                            amount,
+                            unit,
+                            slash,
+                            scaling,
+                            level] = durationRegex;
+                        let formula = "";
+                        if (slash) {
+                            formula = amount >  1 ? `@details.cl.value*${amount}` : `@details.cl.value`;
+                            if (scaling > 0) formula = `floor(${formula}/${scaling})`;
+                        } else {
+                            formula = amount;
+                        }
+                        const classMatch = duration?.value?.match(/per (biohacker|envoy|evolutionist|mechanic|mystic|nanocyte|operative|precog|solarian|soldier|technomancer|vanguard|witchwarper|character) level/i);
+                        if (classMatch?.[1] && classMatch[1] !== "character") {
+                            formula = `${formula}*@classes.${classMatch[1].toLowerCase()}.levels`;
+                        } else if (classMatch?.[1]) {
+                            formula = `${formula}*@details.level.value`;
+                        }
+                        duration.value = formula;
+                        duration.units = unit;
+                        dirty = true;
+                    }
+
+                    if (duration?.value === "instantaneous") {
+                        duration.units = "instantaneous";
+                        duration.value = "";
+                        dirty = true;
+                    }
+
+                    const area = item?.system.area;
+                    if (area?.shape === "shapable") {
+                        area.shape = "cube";
+                        area.shapable = true;
+                        dirty = true;
+                    }
+
                 }
 
                 if (dirty) {
-                    const output = JSON.stringify(alien, null, 2);
+                    const output = JSON.stringify(item, null, 2);
 
                     fs.writeFileSync(`${path}/${file}`, output);
-                    console.log(`Migrated ${alien.name}`);
+                    console.log(`Migrated ${item.name}`);
                 }
 
             }
