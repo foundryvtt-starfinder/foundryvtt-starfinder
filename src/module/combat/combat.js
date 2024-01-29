739e280fba57f419c3ec525841ac0a71b78cb670
LebombJames <77904738+LebombJames@users.noreply.github.com>
2024-01-29 10:23:40 +0000

    Update src/module/combat/combat.js

--- src/module/combat/combat.js
+++ src/module/combat/combat.js
@@ -629,9 +629,7 @@ export class CombatSFRPG extends Combat {
 
         const difficultyHTML = document.createElement("a");
         difficultyHTML.classList.add("combat-difficulty");
-		if (difficulty) {
-			difficultyHTML.classList.add(difficulty);
-		}
+        if (difficulty) difficultyHTML.classList.add(difficulty);
         if (combatType === 'normal') {
             difficultyHTML.title = `${game.i18n.format("SFRPG.Combat.Difficulty.Tooltip.ClickForDetails")}\n\n${game.i18n.format("SFRPG.Combat.Difficulty.Tooltip.PCs")}: ${diffObject.difficultyData.PCs.length} [${game.i18n.format("SFRPG.Combat.Difficulty.Tooltip.APL")} ${diffObject.difficultyData.APL}]\n${game.i18n.format("SFRPG.Combat.Difficulty.Tooltip.HostileNPCs")}: ${diffObject.difficultyData.enemies.length} [${game.i18n.format("SFRPG.Combat.Difficulty.Tooltip.CR")} ${diffObject.difficultyData.CR}]`;
         } else if (combatType === 'starship') {
