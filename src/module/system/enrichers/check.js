f6da6476b644500d86dc5f0a185d1cb5c8034fd1
LebombJames <sam.marine418@gmail.com>
2024-07-04 01:15:42 +0200

    @Check label type

--- src/module/system/enrichers/check.js
+++ src/module/system/enrichers/check.js
@@ -67,6 +67,28 @@ export default class CheckEnricher extends BaseEnricher {
         };
     }
 
+    get checkType() {
+        const shortName = CheckNameHelper.shortFormName(this.args.type);
+        const C = CONFIG.SFRPG;
+
+        if (shortName in C.skills) return "skill";
+        else if (shortName in C.saves) return "save";
+        else if (shortName in C.abilities) return "ability";
+        else return null;
+    }
+
+    get localizedType() {
+        const C = CONFIG.SFRPG;
+        const { type } = this.args;
+
+        switch (this.checkType) {
+            case "skill": return C.skills[type];
+            case "save": return C.saves[type];
+            case "ability": return C.abilities[type];
+            default: return "";
+        }
+    }
+
     /**
      * @override to check using the 3-letter identifier for the type against the valid types (which are 3 letter identifiers).
      * Inputted types are full names.
@@ -77,6 +99,13 @@ export default class CheckEnricher extends BaseEnricher {
         }
 
         return true;
+    }
+
+    validateName() {
+        const i18nPath = this.checkType === save ? "SFRPG.Save" : "SFRPG.Check";
+        const localizedCheck = game.i18n.localize(i18nPath);
+
+        this.name ||= `${this.localizedType} ${localizedCheck}`;
     }
 
     /**
@@ -103,9 +132,9 @@ export default class CheckEnricher extends BaseEnricher {
         if (!actor) return ui.notifications.error("You must have a token or an actor selected.");
         const id = CheckNameHelper.shortFormName(data.type);
 
-        if      (id in CONFIG.SFRPG.skills)    actor.rollSkill(CheckNameHelper.shortFormName(data.type));
-        else if (id in CONFIG.SFRPG.saves)     actor.rollSave(CheckNameHelper.shortFormName(data.type));
-        else if (id in CONFIG.SFRPG.abilities) actor.rollAbility(CheckNameHelper.shortFormName(data.type));
+        if      (id in CONFIG.SFRPG.skills)    actor.rollSkill(id);
+        else if (id in CONFIG.SFRPG.saves)     actor.rollSave(id);
+        else if (id in CONFIG.SFRPG.abilities) actor.rollAbility(id);
 
     }
 
