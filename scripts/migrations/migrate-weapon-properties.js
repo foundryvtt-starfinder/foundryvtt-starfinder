2172f64b995c5b1f282b4eb10dc525464f547cc7
ian612 <34078802+ian612@users.noreply.github.com>
2024-08-16 10:03:26 -0400

    use modular function for property migration

--- scripts/migrations/migrate-weapon-properties.js
+++ scripts/migrations/migrate-weapon-properties.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const dataPaths = [
+/* const itemDataPaths = [
     "./src/items/equipment",
     "./src/items/archetype-features",
     "./src/items/class-features",
@@ -11,10 +11,18 @@ const dataPaths = [
     "./src/items/theme-features",
     "./src/items/themes",
     "./src/items/universal-creature-rules"
+]; */
+
+const itemDataPaths = [
+    "./src/items/equipment"
+];
+
+const actorDataPaths = [
+    "./src/items/alien-archives"
 ];
 
 try {
-    for (const path of dataPaths) {
+    /* for (const path of actorDataPaths) {
         fs.readdir(path, 'utf8', (err, files) => {
             if (err) throw err;
 
@@ -25,22 +33,41 @@ try {
                 });
                 let clean = false;
 
-                const item = JSON.parse(json);
+                const actor = JSON.parse(json);
+                const items = actor.items;
 
-                const properties = item.system.properties;
-                if (properties) {
-                    const newProperties = {};
-                    for (const property of Object.entries(properties)) {
-                        newProperties[property[0]] = {value: property[1]};
-                        if (property[1]) {
-                            newProperties[property[0]].extension = '';
-                        }
+                const newItems = [];
+                for (const item of actor.items) {
+                    if (item.properties) {
+
                     }
-                    item.system.properties = newProperties;
-                    clean = true;
                 }
 
                 if (clean) {
+                    const output = JSON.stringify(actor, null, 2);
+
+                    fs.writeFileSync(`${path}/${file}`, output);
+                    console.log(`Migrated ${actor.name}`);
+                }
+            }
+        });
+    } */
+
+    for (const path of itemDataPaths) {
+        fs.readdir(path, 'utf8', (err, files) => {
+            if (err) throw err;
+
+            for (const file of files) {
+                const json = fs.readFileSync(`${path}/${file}`, {
+                    encoding: 'utf8',
+                    flag: 'r+'
+                });
+
+                const item = JSON.parse(json);
+                const propertyOuts = propertyReplace(item.system.properties);
+
+                if (propertyOuts[0]) {
+                    item.system.properties = propertyOuts[1];
                     const output = JSON.stringify(item, null, 2);
 
                     fs.writeFileSync(`${path}/${file}`, output);
@@ -51,6 +78,22 @@ try {
         });
 
     }
+
 } catch (err) {
     console.log(err);
+}
+
+function propertyReplace(properties) {
+
+    if (properties) {
+        const newProperties = {};
+        for (const property of Object.entries(properties)) {
+            newProperties[property[0]] = {value: property[1]};
+            if (property[1]) {
+                newProperties[property[0]].extension = '';
+            }
+        }
+        return [true, newProperties];
+    }
+    return [false, {}];
 }
