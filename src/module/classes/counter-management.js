9181fa6ccb7c1188cab4bd5d65bf2a2386f304a2
ian612 <34078802+ian612@users.noreply.github.com>
2025-01-01 23:39:17 -0500

    revert to left justification for resources

--- src/module/classes/counter-management.js
+++ src/module/classes/counter-management.js
@@ -104,9 +104,7 @@ export default class CounterManagement {
                 $combatantHtml.find('.token-initiative').detach()
                     .appendTo($div);
 
-                // $combatantHtml.find('.token-resource').addClass('flexcol');
-
-                $div.after('<div id="counter-tokens" class="flexrow"></div>');
+                $div.after('<div id="counter-tokens" class=""></div>');
                 const $counterTokens = $combatantHtml.find('#counter-tokens');
                 $counterTokens.append(additionalHtml);
 
