foundryvtt-starfinder/foundryvtt-starfinder

commit 3ade256f1e444d972eb24b2b43eb27ad5d9ceb45

Stop pretending anyone cares about the results of `RollTree.buildRoll`
I found exactly one segment of code checking the data passed back up
from `buildRoll`, and that was `ItemSFRPG.rollConsumable` wanting to
know if it should consume the item or not. It did this by checking if
the returned data had a truthy `callbackResult` field. Considering that
this part of the code had no knowledge of the callback, it was better to
just pass a success/fail boolean back up instead of all that data.

The large footprint of this change is a consequence of me ensuring that
there are explicit breaks of that data dependency up the call-stack from
`buildRoll()`. I got a bit aggressive making sure the upstream functions
returned a `Promise<void>` wherever possible.

src/module/actor/actor.js
@@ -534,15 +534,13 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
         const skl = this.system.skills[skillId];
 
         if (!this.hasPlayerOwner) {
-            return await this.rollSkillCheck(skillId, skl, options);
-        }
-
-        if (skl.isTrainedOnly && !(skl.ranks > 0)) {
+            await this.rollSkillCheck(skillId, skl, options);
+        } else if (skl.isTrainedOnly && !(skl.ranks > 0)) {
             const content = game.i18n.format(
                 "SFRPG.SkillTrainedOnlyDialog.Content", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)], name: this.name }
             );
 
-            return new Promise(resolve => {
+            await new Promise(resolve => {
                 new Dialog({
                     title: game.i18n.format(
                         "SFRPG.SkillTrainedOnlyDialog.Title", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)] }
@@ -551,7 +549,10 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
                     buttons: {
                         yes: {
                             label: game.i18n.localize("Yes"),
-                            callback: () => resolve(this.rollSkillCheck(skillId, skl, options))
+                            callback: () => {
+                                this.rollSkillCheck(skillId, skl, options)
+                                    .then(() => resolve());
+                            }
                         },
                         cancel: {
                             label: game.i18n.localize("No")
@@ -561,7 +562,7 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
                 }).render(true);
             });
         } else {
-            return await this.rollSkillCheck(skillId, skl, options);
+            await this.rollSkillCheck(skillId, skl, options);
         }
     }
 
@@ -582,7 +583,7 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
 
         parts.push(`@abilities.${abilityId}.abilityCheckBonus`);
 
-        return await DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -604,14 +605,14 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
      * @param {String} saveId The save id (e.g. "will")
      * @param {Object} options Options which configure how saves are rolled
      */
-    rollSave(saveId, options = {}) {
+    async rollSave(saveId, options = {}) {
         const label = CONFIG.SFRPG.saves[saveId];
 
         const rollContext = RollContext.createActorRollContext(this);
 
         const parts = [`@attributes.${saveId}.bonus`];
 
-        return DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -635,7 +636,7 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
             ? game.i18n.format("SFRPG.Rolls.Dice.SkillCheckTitleWithProfession", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)], profession: skill.subname })
             : game.i18n.format("SFRPG.Rolls.Dice.SkillCheckTitle", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)] });
 
-        return await DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -695,7 +696,7 @@ export class ActorSFRPG extends Mix(Actor).with(ActorConditionsMixin, ActorCrewM
 
         this.setupRollContexts(rollContext);
 
-        return await DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,

src/module/dice.js
@@ -161,6 +161,7 @@ export class DiceSFRPG {
     * @param {Number}               [data.fumble]      The value of d20 result which represents a critical failure
     * @param {onD20DialogClosed}    data.onClose       Callback for actions to take when the dialog form is closed
     * @param {DialogOptions}        data.dialogOptions Modal dialog options
+    * @Returns {Promise<void>}
     */
     static async d20Roll({ event = new Event(''), parts, rollContext, title, speaker, flavor, advantage = true, rollOptions = {},
         critical = 20, fumble = 1, chatMessage = true, onClose, dialogOptions, actorContextKey = "actor" }) {
@@ -203,7 +204,7 @@ export class DiceSFRPG {
         const formula = parts.map(partMapper).join(" + ");
 
         const tree = new RollTree(options);
-        return await tree.buildRoll(formula, rollContext, async (button, rollMode, unusedFinalFormula, node, rollMods, bonus = null) => {
+        await tree.buildRoll(formula, rollContext, async (button, rollMode, unusedFinalFormula, node, rollMods, bonus = null) => {
             if (button === "cancel") {
                 if (onClose) {
                     onClose(null, null, null);
@@ -299,7 +300,6 @@ export class DiceSFRPG {
 
             return roll;
         });
-
     }
 
     /**
@@ -452,14 +452,14 @@ export class DiceSFRPG {
     * @param {string}               data.flavor        Any flavor text associated with this roll
     * @param {onDamageDialogClosed} data.onClose       Callback for actions to take when the dialog form is closed
     * @param {Object}               data.dialogOptions Modal dialog options
-    * @returns {Promise<?Roll>}
+    * @returns {Promise<bool>}                         `true` if roll was performed, `false` if it was canceled
     */
     static async damageRoll({ event = new Event(''), parts, criticalData, rollContext, title, speaker, flavor, chatMessage = true, onClose, dialogOptions }) {
         flavor = `${title || ""}${(flavor ? " - " + flavor : "")}`;
 
         if (!rollContext?.isValid()) {
             console.log(['Invalid rollContext', rollContext]);
-            return null;
+            return false;
         }
 
         /** New roll formula system */

src/module/item/item.js
@@ -859,7 +859,8 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
 
         const actorData = this.actor.system;
         if (!this.hasAttack) {
-            return ui.notifications.error("You may not make an Attack Roll with this Item.");
+            ui.notifications.error("You may not make an Attack Roll with this Item.");
+            return;
         }
 
         if (this.type === "starshipWeapon") return this._rollStarshipAttack(options);
@@ -965,7 +966,7 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
         parts.push("@additional.modifiers.bonus");
 
         // Call the roll helper utility
-        return DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             rollContext: rollContext,
@@ -1142,7 +1143,7 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
             parts.push(`@ship.attributes.systems.powerCore.modOther`);
         }
 
-        return await DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             rollContext: rollContext,
@@ -1191,7 +1192,7 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
         rollContext.addContext("weapon", this, this);
         rollContext.setMainContext("");
 
-        return await DiceSFRPG.d20Roll({
+        await DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             rollContext: rollContext,
@@ -1225,6 +1226,7 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
     /**
      * Place a damage roll using an item (weapon, feat, spell, or equipment)
      * Rely upon the DiceSFRPG.damageRoll logic for the core implementation
+     * @returns {Promise<bool>}  `true` if roll was performed, `false` if it was canceled
      */
     async rollDamage({ event } = {}, options = {}) {
         const itemData  = this.system;
@@ -1233,7 +1235,8 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
         const isHealing = this.system.actionType === "heal";
 
         if (!this.hasDamage) {
-            return ui.notifications.error("You may not make a Damage Roll with this Item.");
+            ui.notifications.error("You may not make a Damage Roll with this Item.");
+            return;
         }
 
         if (this.type === "starshipWeapon") return this._rollStarshipDamage({ event: event });
@@ -1575,8 +1578,7 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
         if (itemData.actionType && itemData.actionType !== "save") {
             options.flavorOverride = game.i18n.format("SFRPG.Items.Consumable.UseChatMessage", {consumableName: this.name});
 
-            const result = await this.rollDamage({}, options);
-            if (!result.callbackResult) {
+            if (!await this.rollDamage({}, options)) {
                 // Roll was cancelled, don't consume.
                 return;
             }
@@ -1733,7 +1735,10 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
 
         // Get the Actor from a synthetic Token
         const chatCardActor = this._getChatCardActor(card);
-        if (!chatCardActor) return ui.notifications.error("SFRPG.ChatCard.ItemAction.NoActor");
+        if (!chatCardActor) {
+            ui.notifications.error("SFRPG.ChatCard.ItemAction.NoActor");
+            return;
+        }
 
         // Get the Item
         let item = chatCardActor.items.get(card.dataset.itemId);
@@ -2089,7 +2094,10 @@ export class ItemSFRPG extends Mix(Item).with(ItemActivationMixin, ItemCapacityM
      * @returns {Promise<*>} The return value of the macro
      */
     async executeMacroWithContext(macro, scope = {}) {
-        if (!(macro instanceof Macro)) return ui.notifications.error("A macro was not provided!");
+        if (!(macro instanceof Macro)) {
+            ui.notifications.error("A macro was not provided!");
+            return;
+        }
 
         return macro.execute({
             speaker: ChatMessage.getSpeaker({ actor: this.actor, token: this.actor.token }),

src/module/rolls/rolltree.js
@@ -17,7 +17,7 @@ export default class RollTree {
      * @param {string} formula The formula for the Roll
      * @param {RollContext} contexts The data context for this roll
      * @param {onRollBuilt} callback Function called when the Roll is built.
-     * @returns Stuff
+     * @returns {Promise<bool>}  `true` if roll was performed, `false` if it was canceled
      */
     async buildRoll(formula, contexts, callback) {
         this.formula = formula;
@@ -41,7 +41,7 @@ export default class RollTree {
             if (!context) {
                 console.log(`Cannot find context for variable '${variable}', substituting with a 0.`);
                 const regexp = new RegExp(variable, "gi");
-                this.formula = this.formula.replace(regexp, "0");
+                formula = formula.replace(regexp, "0");
             }
         }
 
@@ -60,11 +60,10 @@ export default class RollTree {
             enabledParts = parts?.filter(x => x.enabled);
         }
 
-        let callbackResult = null;
         if (button === null) {
             console.log('Roll was cancelled');
             await callback('cancel', "none", null);
-            return;
+            return false;
         }
 
         for (const [key, value] of Object.entries(this.nodes)) {
@@ -109,7 +108,7 @@ export default class RollTree {
                     if (enabledParts.length > 1) {
                         part.partIndex = game.i18n.format("SFRPG.Damage.PartIndex", {partIndex: partIndex + 1, partCount: enabledParts.length});
                     }
-                    callbackResult = await callback(button, rollMode, finalSectionFormula, part, this.rootNode, this.rollMods, bonus);
+                    await callback(button, rollMode, finalSectionFormula, part, this.rootNode, this.rollMods, bonus);
                 }
             }
         } else {
@@ -133,16 +132,11 @@ export default class RollTree {
             }
 
             if (callback) {
-                callbackResult = await callback(button, rollMode, finalRollFormula, this.rootNode, this.rollMods, bonus);
+                await callback(button, rollMode, finalRollFormula, this.rootNode, this.rollMods, bonus);
             }
         }
 
-        if (this.options.skipUI) {
-            return {button, rollMode, finalRollFormula, callbackResult};
-        }
-        else {
-            return {button, rollMode, bonus, parts, callbackResult};
-        }
+        return true;
     }
 
     populate() {

src/module/actor/sheet/hazard.js
@@ -99,7 +99,7 @@ export class ActorSheetSFRPGHazard extends ActorSheetSFRPG {
             const rollContext = RollContext.createActorRollContext(this.actor);
 
             const name = game.i18n.format("SFRPG.HazardSheet.Rolls.Damage", {name: this.actor.name});
-            return DiceSFRPG.damageRoll({
+            await DiceSFRPG.damageRoll({
                 event: event,
                 rollContext: rollContext,
                 parts: [{ formula: this.actor.system.attributes.damage.value }],

commit b8f29f2eb552f4d65242ce8b5a9e0d5de1992208

Return RollResult objects from Actor/Item roll methods
3ade256 broke someone's macros. On reflection, expecting those methods
to return values is reasonable, so let's provide a proper API for it.

closes #1600

src/module/actor/actor.js
@@ -555,18 +555,19 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
      * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
      * @param {string} skillId      The skill id (e.g. "ins")
      * @param {Object} options      Options which configure how the skill check is rolled
+     * @returns {Promise<RollResult?>}
      */
     async rollSkill(skillId, options = {}) {
         const skl = this.system.skills[skillId];
 
         if (!this.hasPlayerOwner) {
-            await this.rollSkillCheck(skillId, skl, options);
+            return this.rollSkillCheck(skillId, options);
         } else if (skl.isTrainedOnly && !(skl.ranks > 0)) {
             const content = game.i18n.format(
                 "SFRPG.SkillTrainedOnlyDialog.Content", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)], name: this.name }
             );
 
-            await new Promise(resolve => {
+            return new Promise(resolve => {
                 new Dialog({
                     title: game.i18n.format(
                         "SFRPG.SkillTrainedOnlyDialog.Title", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)] }
@@ -576,7 +577,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
                         yes: {
                             label: game.i18n.localize("Yes"),
                             callback: () => {
-                                this.rollSkillCheck(skillId, skl, options)
+                                this.rollSkillCheck(skillId, options)
                                     .then(() => resolve());
                             }
                         },
@@ -588,7 +589,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
                 }).render(true);
             });
         } else {
-            await this.rollSkillCheck(skillId, skl, options);
+            return this.rollSkillCheck(skillId, options);
         }
     }
 
@@ -597,28 +598,20 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
      *
      * @param {String} abilityId The ability id (e.g. "str")
      * @param {Object} options Options which configure how ability tests are rolled
+     * @returns {Promise<RollResult?>}
      */
     async rollAbility(abilityId, options = {}) {
-        const label = CONFIG.SFRPG.abilities[abilityId];
-        const abl = this.system.abilities[abilityId];
-
-        const parts = [];
-        const data = this.getRollData();
-
-        const rollContext = RollContext.createActorRollContext(this);
-
-        parts.push(`@abilities.${abilityId}.abilityCheckBonus`);
-
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
-            rollContext: rollContext,
-            parts: parts,
-            title:  game.i18n.format("SFRPG.Rolls.Dice.AbilityCheckTitle", {label: label}),
+            rollContext: RollContext.createActorRollContext(this),
+            parts: [ `@abilities.${abilityId}.abilityCheckBonus` ],
+            title:  game.i18n.format("SFRPG.Rolls.Dice.AbilityCheckTitle", {label: CONFIG.SFRPG.abilities[abilityId]}),
             flavor: null,
             speaker: ChatMessage.getSpeaker({ actor: this }),
             chatMessage: options.chatMessage,
             onClose: options.onClose,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -632,6 +625,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
      *
      * @param {String} saveId The save id (e.g. "will")
      * @param {Object} options Options which configure how saves are rolled
+     * @returns {Promise<RollResult?>}
      */
     async rollSave(saveId, options = {}) {
         const label = CONFIG.SFRPG.saves[saveId];
@@ -640,7 +634,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
 
         const parts = [`@attributes.${saveId}.bonus`];
 
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -650,6 +644,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
             chatMessage: options.chatMessage,
             onClose: options.onClose,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -658,9 +653,17 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
         });
     }
 
-    async rollSkillCheck(skillId, skill, options = {}) {
+    /**
+     * Roll a Skill Check
+     * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
+     * @param {string} skillId      The skill id (e.g. "ins")
+     * @param {Object} options      Options which configure how the skill check is rolled
+     * @returns {Promise<RollResult?>}
+     */
+    async rollSkillCheck(skillId, options = {}) {
         const rollContext = RollContext.createActorRollContext(this);
         const parts = [`@skills.${skillId}.mod`];
+        const skill = this.system.skills[skillId];
 
         const title = skillId.includes('pro')
             ? game.i18n.format("SFRPG.Rolls.Dice.SkillCheckTitleWithProfession", { skill: CONFIG.SFRPG.skills[skillId.substring(0, 3)], profession: skill.subname })
@@ -679,7 +682,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
             tags.push({name: "hasSkillRanks", text: game.i18n.format("SFRPG.SkillUntrained")});
         }
 
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -692,6 +695,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
             chatMessage: options.chatMessage,
             onClose: options.onClose,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -705,6 +709,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
      * Roll the Piloting skill of the pilot of a vehicle
      *
      * @param {Object} options Options which configure how saves are rolled
+     * @returns {Promise<RollResult?>}
      */
     async rollVehiclePilotingSkill(role = null, actorId = null, system = null, options = {}) {
 
@@ -742,7 +747,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
 
         this.setupRollContexts(rollContext);
 
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             rollContext: rollContext,
             parts: parts,
@@ -752,6 +757,7 @@ export class ActorSFRPG extends Mix(foundry.documents.Actor).with(ActorCondition
             chatMessage: options.chatMessage,
             onClose: options.onClose,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             }

src/module/dice.js
@@ -167,7 +167,7 @@ export class DiceSFRPG {
     * @param {DialogOptions}        data.dialogOptions Modal dialog options
     * @param {difficulty}           data.difficulty    Optional parameter for checks
     * @param {displayDifficulty}    data.displayDifficulty    Optional parameter to display check difficulty
-    * @returns {Promise<void>}
+    * @returns {Promise<RollResult?>}
     */
     static async d20Roll({ event = new Event(''), parts, rollContext, title, speaker, flavor, advantage = true, rollOptions = {},
         critical = 20, fumble = 1, chatMessage = true, onClose, dialogOptions, actorContextKey = "actor",
@@ -214,6 +214,7 @@ export class DiceSFRPG {
             if (onClose) {
                 onClose(null, null, null);
             }
+            return null;
         } else {
             let dieRoll = "1d20";
             if (rollInfo.button === "disadvantage") {
@@ -315,6 +316,8 @@ export class DiceSFRPG {
             if (errorToThrow) {
                 throw errorToThrow;
             }
+
+            return { roll, finalFormula };
         }
     }
 

src/module/item/item.js
@@ -859,6 +859,8 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
      * Supported options:
      * disableDamageAfterAttack: If the system setting "Roll damage with attack" is enabled, setting this flag to true will disable this behavior.
      * disableDeductAmmo: Setting this to true will prevent ammo being deducted if applicable.
+     *
+     * @returns {Promise<RollResult?>}
      */
     async rollAttack(options = {}) {
         const itemData = this.system;
@@ -973,7 +975,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
         parts.push("@additional.modifiers.bonus");
 
         // Call the roll helper utility
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             actorContextKey: "owner",
@@ -989,6 +991,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             chatMessage: options.chatMessage,
             rollOptions: rollOptions,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -1092,6 +1095,8 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
      * Supported options:
      * disableDamageAfterAttack: If the system setting "Roll damage with attack" is enabled, setting this flag to true will disable this behavior.
      * disableDeductAmmo: Setting this to true will prevent ammo being deducted if applicable.
+     *
+     * @returns {Promise<RollResult?>}
      */
     async _rollStarshipAttack(options = {}) {
         let parts = [];
@@ -1151,7 +1156,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             parts.push(`@ship.attributes.systems.powerCore.modOther`);
         }
 
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             rollContext: rollContext,
@@ -1160,6 +1165,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             critical: 20,
             chatMessage: options.chatMessage,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -1185,6 +1191,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
     /**
      * Place an attack roll for a vehicle using an item.
      * @param {Object} options Options to pass to the attack roll
+     * @returns {Promise<RollResult?>}
      */
     async _rollVehicleAttack(options = {}) {
 
@@ -1200,7 +1207,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
         rollContext.addContext("weapon", this, this);
         rollContext.setMainContext("");
 
-        await DiceSFRPG.d20Roll({
+        return DiceSFRPG.d20Roll({
             event: options.event,
             parts: parts,
             rollContext: rollContext,
@@ -1209,6 +1216,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             critical: 20,
             chatMessage: options.chatMessage,
             dialogOptions: {
+                skipUI: options.skipUI,
                 left: options.event ? options.event.clientX - 80 : null,
                 top: options.event ? options.event.clientY - 80 : null
             },
@@ -1362,6 +1370,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             speaker: ChatMessage.getSpeaker({ actor: this.actor }),
             chatMessage: options.chatMessage,
             dialogOptions: {
+                skipUI: options.skipUI,
                 width: 400,
                 top: event ? event.clientY - 80 : null,
                 left: window.innerWidth - 710
@@ -1506,6 +1515,7 @@ export class ItemSFRPG extends Mix(foundry.documents.Item).with(ItemActivationMi
             speaker: ChatMessage.getSpeaker({ actor: this.actor }),
             chatMessage: options.chatMessage,
             dialogOptions: {
+                skipUI: options.skipUI,
                 width: 400,
                 top: event ? event.clientY - 80 : null,
                 left: window.innerWidth - 710

src/module/rolls/rolltree.js
@@ -58,6 +58,7 @@ export default class RollTree {
      * @param {string} request.mode
      * @param {string} [request.bonus]
      * @param {DamagePart[]} [request.enabledParts]
+     * @returns {RollInfo}
      */
     #processRollRequest(request) {
         const result = {