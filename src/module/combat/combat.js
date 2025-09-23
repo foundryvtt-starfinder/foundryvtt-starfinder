e951064b4b03870f5ab563355584366a6013daeb
Ian Bennett <34078802+ian612@users.noreply.github.com>
2025-09-23 07:39:05 -0400

    Automatically select combat type when GM adds actors to combat (#1662)
    * Automatically select combat type when GM adds actors to combat
    
    * Only switch combat type if combat isn't started
    
    * Update JSDoc
    
    ---------
    
    Co-authored-by: ian612 <spacemanian@icloud.com>
    Co-authored-by: LebombJames <sam.marine418@gmail.com>

--- src/module/combat/combat.js
+++ src/module/combat/combat.js
@@ -1,8 +1,8 @@
 import { CombatDifficulty } from "../apps/combat-difficulty.js";
+import { SFRPG } from "../config.js";
 import { DiceSFRPG } from "../dice.js";
 import RollContext from "../rolls/rollcontext.js";
-import { SFRPG } from "../config.js";
-
+/**  @import Combatant from "@client/documents/combatant.mjs" */
 /*
 The following hooks were added:
 "onBeginCombat", one argument, type object, contains all event data
@@ -899,6 +899,27 @@ export class CombatSFRPG extends foundry.documents.Combat {
         }
     }
 
+    /**
+     * Changes the combat type when the GM adds a combatant to the appropriate one for that
+     * combatant's actor type.
+     * @param {Combatant} combatant
+     * @override
+     */
+    _onEnter(combatant) {
+        if (game.user.isGM && combatant.actor && !this.started) {
+            switch (combatant.actor.type) {
+                case "starship":
+                    this.update({"flags.sfrpg.combatType": "starship"});
+                    break;
+                case "vehicle":
+                    this.update({"flags.sfrpg.combatType": "vehicleChase"});
+                    break;
+                default:
+                    this.update({"flags.sfrpg.combatType": "normal"});
+                    break;
+            }
+        }
+    }
 }
 
 async function onConfigClicked(combat, direction) {
