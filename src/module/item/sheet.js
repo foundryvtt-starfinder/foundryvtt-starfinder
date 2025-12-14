a774c9faef7124e9406300de2d67bbb453caf78b
Danimrath <anthonyawade@gmail.com>
2025-12-13 22:51:26 -0600

    Add null check to _computeItemSaveBonus

--- src/module/item/sheet.js
+++ src/module/item/sheet.js
@@ -311,11 +311,10 @@ export class ItemSheetSFRPG extends foundry.appv1.sheets.ItemSheet {
     _computeItemSaveBonus() {
         // TODO: Move this into the item's calculation rather than calculating on the sheet
         const parentItem = this.item;
-        const parentActor = parentItem.actor;
+        const parentActorAbilities = parentItem.actor?.system?.abilities;
         const itemLevel = parentItem.system.level;
-        if (parentActor) {
-            const ActorAbilities = parentActor.system.abilities;
-            return `[F: ${Math.max(itemLevel, ActorAbilities.con.mod, 0)}, R: ${Math.max(itemLevel, ActorAbilities.dex.mod, 0)}, W: ${Math.max(itemLevel, ActorAbilities.dex.mod, 0)}]`;
+        if (parentActorAbilities) {
+            return `[F: ${Math.max(itemLevel, parentActorAbilities.con.mod, 0)}, R: ${Math.max(itemLevel, parentActorAbilities.dex.mod, 0)}, W: ${Math.max(itemLevel, parentActorAbilities.dex.mod, 0)}]`;
         } else if (itemLevel < 1) {
             return 0;
         } else {
